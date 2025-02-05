64d0e842d69fd6cd05978f91c6753bd633e0bffe
Johan Belusca <vadimmarquez2@gmail.com>
2025-02-05 11:14:36 +0100

    updated gitignore, ajout projeterPoint pour l'origine

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import math
 import os
 
+np.set_printoptions(suppress=True)
 
-print(os.getcwd())
 
-fileLocation = 'sequences_imgs/'
+fileLocation = '/home/burei/Documents/IUT/robotique_s6/PHOTOS_SAE/sequences_imgs'
 sequence = 'imgs2024-03-03_17_49_28.135995R'
-fileName = os.getcwd() + "/" + fileLocation + sequence + '/' + 'CalibResult.npz'
+fileName = fileLocation + '/' + sequence + '/' + 'CalibResult.npz'
+
+pictureName = fileLocation + '/' + sequence +  '/' + 'im_00000R.png'
 
 dimSquare=0.012
 with np.load(fileName) as X:
@@ -26,13 +28,43 @@ print(
        "pu", pu, "\n",
        "pv", pv, "\n"
     )
- 
-for i in range(nbimages):  
+
+iCc=np.float32([[alphau,0,pu,0],[0,alphav,pv,0],[0,0,1,0]])
+
+
+def projeterPoint(M, iCw):
+    print("M:"+str(M))
+    M = np.append(M, 1)
+    print("M:"+str(M))
+    m=iCw1@M
+
+    mu=m[0]/m[2]
+    mv=m[1]/m[2] 
+
+    mu=int(round(mu,0))
+    mv=int(round(mv,0))
+    print("mu:"+str(mu))
+    print("mv:"+str(mv))   
+    return  (mu,mv)
+
+iCw1 = None
+
+for i in range(nbimages):
+    
     matroti, jacobian=cv2.Rodrigues(rvecs[i]) #Rodrigues permet de calculer la matrice de rotation correspondante    
     axis = np.float32([[0,0,0], [3*dimSquare,0,0], [0,3*dimSquare,0], [0,0,3*dimSquare]]).reshape(-1,3)
     imgpts, jac = cv2.projectPoints(axis, rvecs[i], tvecs[i], mtx,None) #pour ne pas utiliser les distorsions
     cRTw=np.concatenate((matroti,tvecs[i]),axis=1)
     cRTw = np.vstack((cRTw,[0,0,0,1]))
     print("cRTw: "+str(cRTw))
+    iCw=iCc@cRTw
+    if i == 0:
+      iCw1 = iCw
+    print("iCw :" + str(iCw))
 
+img = cv2.imread(pictureName, cv2.IMREAD_COLOR)
+cv2.circle(img, projeterPoint((0,0,0), iCw1), 1, (255,0,0), 3)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
     
