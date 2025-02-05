6aaa8f66f54aa709c683644a17e545a76a541bc0
Johan Belusca <vadimmarquez2@gmail.com>
2025-02-05 17:02:44 +0100

    projeter point + disance au plan vertical OK

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ import os
 np.set_printoptions(suppress=True)
 
 
+def compareFloat(f1, f2):
+    if f1 == 0 :
+      return abs(f1-f2) < 1e-6
+    return (abs((f1-f2))/f1)<1e-6
+
 fileLocation = '/home/burei/Documents/IUT/robotique_s6/PHOTOS_SAE/sequences_imgs'
 sequence = 'imgs2024-03-03_17_49_28.135995R'
 fileName = fileLocation + '/' + sequence + '/' + 'CalibResult.npz'
@@ -33,18 +38,18 @@ iCc=np.float32([[alphau,0,pu,0],[0,alphav,pv,0],[0,0,1,0]])
 
 
 def projeterPoint(M, iCw):
-    print("M:"+str(M))
+    #print("M:"+str(M))
     M = np.append(M, 1)
-    print("M:"+str(M))
-    m=iCw1@M
+    #print("M:"+str(M))
+    m=iCw@M
 
     mu=m[0]/m[2]
     mv=m[1]/m[2] 
 
     mu=int(round(mu,0))
     mv=int(round(mv,0))
-    print("mu:"+str(mu))
-    print("mv:"+str(mv))   
+    #print("mu:"+str(mu))
+    #print("mv:"+str(mv))   
     return  (mu,mv)
 
 iCw1 = None
@@ -56,14 +61,29 @@ for i in range(nbimages):
     imgpts, jac = cv2.projectPoints(axis, rvecs[i], tvecs[i], mtx,None) #pour ne pas utiliser les distorsions
     cRTw=np.concatenate((matroti,tvecs[i]),axis=1)
     cRTw = np.vstack((cRTw,[0,0,0,1]))
-    print("cRTw: "+str(cRTw))
     iCw=iCc@cRTw
     if i == 0:
       iCw1 = iCw
-    print("iCw :" + str(iCw))
+      print(iCw1.shape)
 
 img = cv2.imread(pictureName, cv2.IMREAD_COLOR)
-cv2.circle(img, projeterPoint((0,0,0), iCw1), 1, (255,0,0), 3)
+orig, X, Y, Z= projeterPoint((0,0,0), iCw1), projeterPoint((0.1,0,0), iCw1), projeterPoint((0,0.1,0), iCw1), projeterPoint((0,0,0.1), iCw1)
+
+for x in range(5000):
+  point = projeterPoint((-x*0.00001,0,0), iCw1)
+
+   #print(point[0], 1.252 * point[0] + 116, point[1])
+   
+  if (1.252 * point[0] + 116 < point[1]):
+      print(x)
+      break
+   #cv2.circle(img, point, 1, (255,0,0), 1)
+   #if compareFloat(1.252*point[0] + 116, point[1]):
+      #print("intersection at :", point)
+
+cv2.line(img, orig, X, (0,0,255), 3)
+cv2.line(img, orig, Y, (0,255,0), 3)
+cv2.line(img, orig, Z, (255,0,0), 3)
 cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
