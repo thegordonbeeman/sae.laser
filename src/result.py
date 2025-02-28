31d588b613284b72625e573c61e43110c91fb395
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2025-02-28 19:57:02 +0100

    Update result.py

--- src/result.py
+++ src/result.py
@@ -3,5 +3,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Nombre de points à afficher
+SAMPLE_COUNT = 5000
+
 array = np.load(os.path.join(os.getcwd(), "result.npy")
 
+_is = np.random.choice(array.shape[1], SAMPLE_COUNT)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(array[0, _is], array[1, _is], array[2, _is])
+
+ax.set_aspect('equal')
+plt.show()
